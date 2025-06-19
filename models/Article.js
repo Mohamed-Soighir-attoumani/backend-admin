4bef422ccafd13078234c86eb7bb7835d69161df
Mohamed-Soighir-attoumani <131267845+Mohamed-Soighir-attoumani@users.noreply.github.com>
2025-06-19 23:17:39 +0300

    Article.js

--- models/Article.js
+++ models/Article.js
@@ -1,21 +1,10 @@
+// models/Article.js
 const mongoose = require('mongoose');
 
-const articleSchema = new mongoose.Schema(
-  {
-    title: {
-      type: String,
-      required: true,
-    },
-    content: {
-      type: String,
-      required: true,
-    },
-    imageUrl: {
-      type: String,
-      default: '', // facultatif
-    },
-  },
-  { timestamps: true } // pour createdAt et updatedAt
-);
+const articleSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  imageUrl: { type: String, default: null }, // ✅ pour Cloudinary
+}, { timestamps: true });
 
 module.exports = mongoose.model('Article', articleSchema);
