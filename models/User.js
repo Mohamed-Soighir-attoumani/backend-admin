6709c3536d09498db790980fd75f3bdaf5519db4
Mohamed-Soighir-attoumani <131267845+Mohamed-Soighir-attoumani@users.noreply.github.com>
2025-08-20 22:44:55 +0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,12 +1,13 @@
-// backend/models/User.js
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema(
   {
-    email: { type: String, required: true, unique: true, index: true },
-    password: { type: String, required: true, select: false },
-    role: { type: String, default: 'admin' },
-    // name: { type: String }, // si tu veux
+    email:       { type: String, required: true, unique: true, index: true },
+    password:    { type: String, required: true, select: false },
+    role:        { type: String, default: 'admin' },
+    name:        { type: String, default: '' },         // ⬅️ nom affichable
+    communeName: { type: String, default: '' },         // ⬅️ nom de la commune (optionnel)
+    photo:       { type: String, default: '' },         // ⬅️ avatar (optionnel)
   },
   { timestamps: true }
 );
